5e89edd645a94dcb0b2be24d6470b930791f672f
msalexford <alefor1@gmail.com>
2024-12-09 19:38:37 -0600

    fix link

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/scotus-evolution/",
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+    outDir: "dist",
+    copyPublicDir: true,
   },
 });
