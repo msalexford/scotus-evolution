f1080f983a234baa02446686ea32aee4e13d577a
msalexford <alefor1@gmail.com>
2024-12-09 19:27:38 -0600

    relink

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/scotus-evolution/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
